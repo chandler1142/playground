794c9a5ff987f353b46ee22d72b1e6a93887d9e9
chandler1142 <thzw1142@163.com>
2018-05-06 11:07:44 +0800

    bootstrap znode

---
+++ src/main/java/com/chandler/zookeeper/chapter3/BootStrap.java
@@ -0,0 +1,70 @@
+package com.chandler.zookeeper.chapter3;
+
+
+import org.apache.zookeeper.*;
+import org.slf4j.*;
+
+import java.io.IOException;
+
+public class BootStrap implements Watcher {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BootStrap.class);
+
+    ZooKeeper zk;
+    String hostPort;
+
+    BootStrap(String hostPort) {
+        this.hostPort = hostPort;
+    }
+
+    void start() throws IOException {
+        zk = new ZooKeeper(hostPort, 15000, this);
+    }
+
+    public void bootstrap() {
+        createParent("/workers", new byte[0]);
+        createParent("/status", new byte[0]);
+        createParent("/tasks", new byte[0]);
+        createParent("/assign", new byte[0]);
+    }
+
+    void createParent(String path, byte[] data) {
+        zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, createParentCallback, data);
+    }
+
+    AsyncCallback.StringCallback createParentCallback = new AsyncCallback.StringCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)) {
+                case CONNECTIONLOSS:
+                    createParent(path, (byte[]) ctx);
+                    break;
+                case OK:
+                    LOG.info("Parent created");
+                    break;
+                case NODEEXISTS:
+                    LOG.warn("Parent already registered: " + path);
+                    break;
+                default:
+                    LOG.error("Something went wrong: ", KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
+
+    @Override
+    public void process(WatchedEvent event) {
+        System.out.println(event);
+    }
+
+    void close() throws InterruptedException {
+        zk.close();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BootStrap bootStrap = new BootStrap("127.0.0.1:2181");
+        bootStrap.start();
+        bootStrap.bootstrap();
+        Thread.sleep(60000);
+        bootStrap.close();
+    }
+}
